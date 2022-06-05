99741577e3dc7a2b65fea7d5591db7dc504fbfe3
Rafael Araujo <rafael3124@gmail.com>
2022-06-05 11:50:59 -0300

    Create SistemaCadastroEvento.js

---
+++ SistemaCadastroEvento.js
@@ -0,0 +1,14 @@
+let DataEvento = new Date('12/01/2022')
+let DataAtual = new Date()
+
+if (DataEvento > DataAtual){
+    consloe.log ("evento válido")
+}else{
+    console.log ("Cadastro não permitido")
+}
+
+
+
+
+
+
